c8e6b1bc3d52102049a9773361ea1a18666b3a41
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-05-04 20:41:49 +0100

    Destroy projectile and target on contact

---
+++ Assets/Scripts/DestroyOnContact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class DestroyOnContact : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
