ce7c040b253de0773c4189f083f0b4a12a2be4cd
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-05-04 19:05:13 +0100

    Spawn projectiles on input

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public bool actionButton;
+
+    void Update()
+    {
+        actionButton = Input.anyKeyDown;
+    }
+}
---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+    }
+}
