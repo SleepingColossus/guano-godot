17966dca0fb27fa0b79cf1fe49bfdda2e53fbbb8
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-05-04 17:47:32 +0100

    Spawn buses

---
+++ Assets/Scripts/MovingTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    public Vector2 velocity;
+    private Rigidbody2D _body;
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _body.velocity = velocity;
+
+        if (transform.position.x < -10.0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TargetSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public float delay = 2.0f;
+    public bool active = true;
+
+    void Start()
+    {
+        StartCoroutine(TargetGenerator());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator TargetGenerator()
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (active)
+        {
+            var prefab = prefabs[Random.Range(0, prefabs.Length)];
+            var newTrasform = transform;
+
+            Instantiate(prefab, newTrasform.position, Quaternion.identity);
+        }
+
+        StartCoroutine(TargetGenerator());
+    }
+}
