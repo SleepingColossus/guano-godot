92d7adbc9ff99b18d206ca003ff9a0c15f7dda39
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-05-04 16:29:20 +0100

    Add scrolling background

---
+++ Assets/Scripts/AnimatedBackground.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedBackground : MonoBehaviour
+{
+    public Vector2 velocity;
+    private Rigidbody2D _body;
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _body.velocity = velocity;
+
+        if (transform.position.x < -12.8f)
+        {
+            transform.position = new Vector3(11.2f, transform.position.y, transform.position.z);
+        }
+    }
+}
