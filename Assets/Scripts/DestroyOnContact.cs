52960fdf6c23d1f3b303f4653a9913517d061b9c
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-05-05 15:40:07 +0100

    Add remaining targets

--- Assets/Scripts/DestroyOnContact.cs
+++ Assets/Scripts/DestroyOnContact.cs
@@ -5,12 +5,13 @@ public class DestroyOnContact : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        var points = other.GetComponent<Reward>().pointsAwarded;
+        var reward = other.GetComponent<Reward>();
 
         // find better solution
         var gameState = GameObject.Find("GameState").GetComponent<GameState>();
-        gameState.UpdateScore(points);
-        
+        gameState.UpdateScore(reward.pointsAwarded);
+        gameState.UpdateAmmo(reward.ammoAwarded);
+
         other.GetComponent<MovingTarget>().Die();
         Destroy(gameObject);
     }
