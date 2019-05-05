65eae42f0a441a7dcd8e97b566d805cb466b7654
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-05-05 20:45:51 +0100

    Hint updates

---
+++ Assets/Scripts/HideHintText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideHintText : MonoBehaviour
+{
+    private Text _hintText;
+    public float hideDelay;
+
+    private void Awake()
+    {
+        _hintText = GetComponent<Text>();
+        StartCoroutine(HideText());
+    }
+
+    private IEnumerator HideText()
+    {
+        yield return new WaitForSeconds(hideDelay);
+        _hintText.enabled = false;
+    }
+}
