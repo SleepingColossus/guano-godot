9833cfbd7847f3cbfc7500dc8b2ea8aae56da042
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2019-05-05 20:19:53 +0100

    Play sounds on game over

--- Assets/Scripts/FallingMotion.cs
+++ Assets/Scripts/FallingMotion.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FallingMotion : MonoBehaviour
 {
     private Rigidbody2D _body;
     private Animator _animator;
-    
+
+    private AudioSource _audioSource;
+    public AudioClip fallingSound;
+    public AudioClip landSound;
+
     public Vector2 velocity;
     public float angularVelocity;
+    private bool _landed;
 
     private void Start()
     {
@@ -16,13 +20,22 @@ public class FallingMotion : MonoBehaviour
         _body.angularVelocity = angularVelocity;
 
         _animator = GetComponent<Animator>();
+
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = fallingSound;
+        _audioSource.Play();
     }
 
     private void LateUpdate()
     {
-        if (transform.position.y < -2)
+        if (!_landed && transform.position.y < -2)
         {
             _animator.enabled = false;
+
+            _audioSource.clip = landSound;
+            _audioSource.Play();
+
+            _landed = true;
         }
     }
 }
