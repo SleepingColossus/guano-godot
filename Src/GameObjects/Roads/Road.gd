12e68ec64a1ec88680dd6ed70fd3ec4e158a609d
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-01-23 16:46:57 +0000

    Destroy roads offscreen

--- Src/GameObjects/Roads/Road.gd
+++ Src/GameObjects/Roads/Road.gd
@@ -2,8 +2,13 @@ extends CharacterBody2D
 
 @export var horizontal_velocity : int
 
+var sprite_width := 300
+
 func _ready():
     velocity = Vector2(horizontal_velocity, 0)
 
 func _process(delta):
     move_and_slide()
+
+    if position.x < -sprite_width:
+        queue_free()
