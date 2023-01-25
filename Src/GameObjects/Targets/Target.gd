c4ef8397a10001f455305f8017786ad5b050fdfb
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-01-25 06:28:26 +0000

    Add basic death animation upon collision

--- Src/GameObjects/Targets/Target.gd
+++ Src/GameObjects/Targets/Target.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var movement_speed : int
 
+@onready var collider : CollisionShape2D = $CollisionShape2D
+
 var destroy_offset := 200
 
 func _ready():
@@ -12,3 +14,7 @@ func _process(delta):
 
     if global_position.x < -destroy_offset:
         queue_free()
+
+func die():
+    collider.disabled = true
+    queue_free()
