8c072814a0071a35b462c99c84beaa5f685e3ec3
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-01-28 07:18:00 +0000

    Add streak and score multiplier

--- Src/GameObjects/Bird/Bird.gd
+++ Src/GameObjects/Bird/Bird.gd
@@ -3,7 +3,8 @@ class_name Bird
 extends AnimatedSprite2D
 
 signal shot_fired(ammo_remaining: int)
-signal rewards_collected(score: int, ammo: int)
+signal rewards_collected(score: int, ammo: int, streak: int, multiplier: int)
+signal streak_broken(streak: int, multiplier: int)
 
 @export var projectile : PackedScene
 @export var ammo := 8
@@ -12,12 +13,18 @@ signal rewards_collected(score: int, ammo: int)
 var ready_to_fire := true
 
 var score := 0
+var score_multiplier := 1
+var streak := 0
 
 # containers for projectile instances
 # used in order to detach projectiles from player's coordinate system
 @onready var container = get_tree().get_root()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+    # initialize UI
+    # rewards_collected.emit(score, ammo, streak, score_multiplier)
+    pass
+
 func _process(_delta):
     if Input.is_anything_pressed() && ready_to_fire && ammo > 0:
         ammo -= 1
@@ -31,10 +38,21 @@ func _process(_delta):
         shot_fired.emit(ammo)
 
 func target_hit(target: Target):
-    score += target.score_awarded
+    streak += 1
+    score_multiplier = adjust_multiplier(streak)
+
+    score += target.score_awarded * score_multiplier
     ammo += target.ammo_awarded
 
-    rewards_collected.emit(score, ammo)
+    rewards_collected.emit(score, ammo, streak, score_multiplier)
+
+func target_missed():
+    streak = 0
+    score_multiplier = adjust_multiplier(streak)
+
+func adjust_multiplier(streak: int) -> int:
+    var x = (streak / 10) + 1
+    return max(x, 1)
 
 func _on_cooldown_timeout():
     ready_to_fire = true
--- Src/Scenes/UIHandler.gd
+++ Src/Scenes/UIHandler.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var score_label := $ScoreLabel
 @onready var ammo_label := $AmmoLabel
+@onready var streak_label := $StreakLabel
+@onready var multiplier_label := $MultiplierLabel
 
 func _ready():
     pass # Replace with function body.
@@ -9,9 +11,11 @@ func _ready():
 func _process(_delta):
     pass
 
-func _on_bird_rewards_collected(score, ammo):
+func _on_bird_rewards_collected(score, ammo, streak, multiplier):
     score_label.text = "Score: %d" % score
     ammo_label.text = "Ammo: %d" % ammo
+    streak_label.text = "Streak: %d" % streak
+    multiplier_label.text = "Multiplier: x%d" % multiplier
 
 func _on_bird_shot_fired(ammo_remaining):
     ammo_label.text = "Ammo: %d" % ammo_remaining
