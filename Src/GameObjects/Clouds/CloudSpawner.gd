d05a0833276c414626517fcab82350a1a9a53787
Nikola Mančić <NikolaMancic@users.noreply.github.com>
2023-01-29 22:01:38 +0000

    Adjust cloud speed & spawn rate

--- Src/GameObjects/Clouds/CloudSpawner.gd
+++ Src/GameObjects/Clouds/CloudSpawner.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var cooldown_min : int
+@export var cooldown_max : int
 @onready var cooldown_timer := $Cooldown
 
 @export var clouds : Array[PackedScene]
@@ -18,6 +20,8 @@ func spawn():
     cloud.global_position = global_position
     container.add_child.call_deferred(cloud)
 
+    var rand_cooldown : int = randi_range(cooldown_min, cooldown_max)
+    cooldown_timer.wait_time = rand_cooldown
     cooldown_timer.start()
 
 func _on_cooldown_timeout():
